e87ff4553470fafe96c192e15911a5f5a3f84065
Alex <oleksandrilnytskyi@gmail.com>
2022-08-13 16:23:54 +0200

    first page

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,7 @@
 let imgSrcArr = ['./img/header-img1.png', './img/header-img2.png']
 
 let sliderImg = document.querySelector('.slider__image-item');
+let secondSliderImg = document.querySelector('.popular__image-item')
 let src = sliderImg.getAttribute('src');
 
 let toggle = false;
@@ -10,15 +11,21 @@ setInterval(() => {
 
     if (toggle) {
         sliderImg.setAttribute('src', imgSrcArr[1])
+        secondSliderImg.setAttribute('src', imgSrcArr[0])
     }else{
         sliderImg.setAttribute('src', imgSrcArr[0])
+        secondSliderImg.setAttribute('src', imgSrcArr[1])
     }
 },5000)
 
 //paginaton
 let count = 1;
 let paginaton = document.querySelectorAll('.slider__pagination-item')
+let secondPagination = document.querySelectorAll('.popular__pagination-item')
+
 paginaton[0].classList.add('active')
+secondPagination[0].classList.add('active')
+
 
 setInterval(() => {
     if (count == 3) {
@@ -27,18 +34,33 @@ setInterval(() => {
     count++;
 
     if (count == 1) {
+        secondPagination.forEach((elem) => {
+            elem.classList.remove('active')
+        })
+        secondPagination[0].classList.add('active')
+
         paginaton.forEach((elem) => {
             elem.classList.remove('active')
         })
         paginaton[0].classList.add('active')
     }
     if (count == 2) {
+        secondPagination.forEach((elem) => {
+            elem.classList.remove('active')
+        })
+        secondPagination[1].classList.add('active')
+
         paginaton.forEach((elem) => {
             elem.classList.remove('active')
         })
         paginaton[1].classList.add('active')
     }
     if (count == 3) {
+        secondPagination.forEach((elem) => {
+            elem.classList.remove('active')
+        })
+        secondPagination[2].classList.add('active')
+
         paginaton.forEach((elem) => {
             elem.classList.remove('active')
         })
