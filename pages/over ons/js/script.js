469a03512b2fda4af755481db11099ba412dc463
Alex <oleksandrilnytskyi@gmail.com>
2022-08-16 10:24:54 +0200

    second

--- pages/over ons/js/script.js
+++ pages/over ons/js/script.js
@@ -1,16 +1,30 @@
-let img = document.querySelector('.info__image-item')
+let imgArr = document.querySelectorAll('.info__image-item')
 
-if (window.innerWidth <= 570) {
-    img.setAttribute('src', './img/info-image2.png');
+if (window.innerWidth < 570) {
+    imgArr.forEach((elem) => {
+        if (elem.getAttribute('src') == './img/info-image.png') {
+            elem.setAttribute('src', './img/info-image2.png')
+        }
+        if (elem.getAttribute('src') == './img/over-image.png'){
+            elem.setAttribute('src', './img/over-image2.png')
+        }
+    })
 }
 
-if(window.innerWidth >= 570) {
-    img.setAttribute('src', './img/info-image.png');
-}
 
 
-//slider
 $(document).ready(function() {
+    //slider info
+    $('.info__image').slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 5000,
+        dots: true,
+    });
+
+
+
+    //slider slick swipe
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -20,3 +34,15 @@ $(document).ready(function() {
         centerPadding: '30vw',
     });
 });
+
+if (window.innerWidth < 570) {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: '30vw',
+        arrows: false,
+    });
+}
